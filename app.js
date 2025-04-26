da2368e905f589c4ebd1ff3e90ae4028067045df
javeriatanveerdeveloper <alihussainuf456@gmail.com>
2025-04-26 11:18:56 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+let age=prompt('enter age of 1st student');
+ age1=parseInt(age);
+console.log('age of 1st student is;' +age1);
+
+let age2=prompt('enter age of 2nd student');
+ age3=parseInt(age2);
+console.log('age of 2nd student is;' +age3);
+
+let age4=prompt('enter age of 3rd student');
+ age5=parseInt(age4);
+console.log('age of 3rd student is;' +age5);
+let age6=prompt('enter age of 4th student');
+ age7=parseInt(age6);
+console.log('age of 4th student is;' +age7);
+
+
+let age8=prompt('enter age of 5th student');
+ age9=parseInt(age8);
+console.log('age of 5th student is;' +age9);
+let age10=prompt('enter age of 6th student');
+ age11=parseInt(age10);
+console.log('age of 6th student is;' +age11);
+let age12=prompt('enter age of 7th student');
+ age13=parseInt(age12);
+console.log('age of 7th student is;' +age13);
+
+let age14=prompt('enter age of 8th student');
+ age15=parseInt(age14);
+console.log('age of 8th student is;' +age15);
+let age16=prompt('enter age of 9th student');
+ age17=parseInt(age16);
+console.log('age of 9th student is;' +age17);
+let age18=prompt('enter age of 10th student');
+ age19=parseInt(age18);
+console.log('age of 10th student is;' +age19);
+
+let avg=(age1 + age3 + age5 + age7 + age9 + age11 + age13 + age15 + age17 + age19) / 10;
+console.log('average is =' +avg);
